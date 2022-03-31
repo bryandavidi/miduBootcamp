a9c19830bd707cb18ee3962d32ef71c9275a3e49
bryandavidi <bryan.ibanez@uptc.edu.co>
2022-03-31 00:42:01 -0500

    Initial commit

---
+++ part1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Mensaje color='red' message='Estamos'/>
+    </div>
+  )
+}
+
+export default App;
